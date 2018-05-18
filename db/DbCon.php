e91cef2d76b5e9c86d9436614a75162bf4e5f9a9
Pranav V R <pvrforpranavvr@gmail.com>
2018-05-18 23:22:27 +0530

    DbCon PDO added

---
+++ db/DbCon.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DbCon
+ *
+ * @author pvr-admin
+ */
+class DbCon {
+
+    private $host = "localhost";
+    private $db_name = "php_rest_api";
+    private $username = "root";
+    private $password = "";
+    public $connection;
+
+    // get the database connection
+    public function getConnection() {
+        $this->connection = null;
+        try {
+            $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->connection->exec("set names utf8");
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Database connection error: " . $exception->getMessage();
+        }
+
+        return $this->connection;
+    }
+
+}
